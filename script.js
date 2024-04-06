a6897c58205d7bb3237d4e3138be54f04a1da340
Sushil0c <139460233+Sushil0c@users.noreply.github.com>
2024-04-06 15:17:40 +0545

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Mock app data
     const mockApps = [
-        { title: "App 1", body: "Description for App 1", mediafireLink: "YOUR_MEDIAFIRE_LINK_APP_1" },
-        { title: "App 2", body: "Description for App 2", mediafireLink: "YOUR_MEDIAFIRE_LINK_APP_2" },
-        { title: "App 3", body: "Description for App 3", mediafireLink: "YOUR_MEDIAFIRE_LINK_APP_3" }
+        { title: "Jieshuo+", body: "whats new 
+Version: 20240401
+Optimized partial system progress bar prompts and operations.
+Improved some software touch experience focusing.
+Enhanced component editing segmentation mode by adding support for single line and double line segmentation.
+Optimized performance and reduced power consumption.
+Added an option to enhance application sound effects.
+Fixed bug with built-in eSpeak adjustment parameters being invalid.
+The super sound effect can now be downloaded for use in movies and TV shows; the selected application will be affected by this sound effect.
+Added a function for super sound effects for individual applications. It creates a new sub-folder under the effect folder in the sound effect program and adds sound effects to it, taking effect after re-applying the sound effect.
+Split movies into segments and assign line numbers for dynamic playback (long press for segment TV design).
+In the Editor, added a function to increase the display of line numbers for editing (long press the line button for quick switch).
+Added a quick switch command for auto-reading messages.
+Added a function to automatically read out message recordings.
+Fixed bug where pulling down the notification bar caused no sound effect.
+Added more example word functions for input method keyboards.
+Increased usage of custom translation engine functions (in advanced settings and voice assistant settings).
+Optimized content black list rules.
+Added description of blacklist keyword format.
+Optimized blacklist settings by increasing direct selection matching mode.
+Fixed abnormal bug when browsing Facebook list items for reading.
+Optimized display effects for timer recording function.", mediafireLink: "https://www.mediafire.com/file/3wr2vjahb1cdc6g/Jieshuo%252B.apk" },
+        { title: "", body: "", mediafireLink: "" },
+        { title: "", body: "", mediafireLink: "" }
     ];
 
     // Iterate through mock app data and display
