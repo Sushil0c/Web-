9bc99c6fa5902c7ebb55d87f0a7335277d26c812
Sushil0c <139460233+Sushil0c@users.noreply.github.com>
2024-04-08 18:44:14 +0545

    Update appdata.js

--- appdata.js
+++ appdata.js
@@ -1,64 +1,18 @@
- export const appData = [
+export const appData = [
     {
         title: "Jieshuo+",
         description: "What's new: Optimized partial system progress bar prompts and operations. Improved some software touch experience focusing. Enhanced component editing segmentation mode by adding support for single line and double line segmentation. Optimized performance and reduced power consumption. Added an option to enhance application sound effects. Fixed bug with built-in eSpeak adjustment parameters being invalid. The super sound effect can now be downloaded for use in movies and TV shows; the selected application will be affected by this sound effect. Added a function for super sound effects for individual applications. It creates a new sub-folder under the effect folder in the sound effect program and adds sound effects to it, taking effect after re-applying the sound effect. Split movies into segments and assign line numbers for dynamic playback (long press for segment TV design). In the Editor, added a function to increase the display of line numbers for editing (long press the line button for quick switch). Added a quick switch command for auto-reading messages. Added a function to automatically read out message recordings. Fixed bug where pulling down the notification bar caused no sound effect. Added more example word functions for input method keyboards. Increased usage of custom translation engine functions (in advanced settings and voice assistant settings). Optimized content black list rules. Added description of blacklist keyword format. Optimized blacklist settings by increasing direct selection matching mode. Fixed abnormal bug when browsing Facebook list items for reading. Optimized display effects for timer recording function.",
         link: "https://www.mediafire.com/file/3wr2vjahb1cdc6g/Jieshuo%252B.apk"
     },
+    {
         title: "Smart AudioBook Player_v10.7.7",
-        description: "What's new
-+ Fixed situation when Sort button was invisible when one of Folder View modes is enabled.
-
-+ Added "Show chapters" to the Settings--Full version settings.
-If by some reason you don't want to see chapters - you can disable them now.
-
-+ On some phones running Android 13 or later, the app could not display notifications and the user could not enable notifications.
-Now, if the app is not showing notifications, you can enable them in:
-Android Settings--Apps--Smart AudioBook Player--Notifications.",
+        description: "What's new: + Fixed situation when Sort button was invisible when one of Folder View modes is enabled. + Added 'Show chapters' to the Settings--Full version settings. If by some reason you don't want to see chapters - you can disable them now. + On some phones running Android 13 or later, the app could not display notifications and the user could not enable notifications. Now, if the app is not showing notifications, you can enable them in: Android Settings--Apps--Smart AudioBook Player--Notifications.",
         link: "https://www.mediafire.com/file/cxfl1hxd2h18mox/Smart+AudioBook+Player-v10.7.7(Premium).apk"
     },
     {
         title: "Smart Tools_v20.9",
-      description: ",Smart Tools - All In One is a useful kit of tools with 40+ carpenter construction measure and other tools and utilities. Use device's in-built sensors in all in one tool box app useful like a swiss army knife.
-
-Measure tools kit:
-dB level: Measure the sound dB level and its spectrum;
-Location (map) with altimeter;
-Distance meter;
-Stopwatch;
-Thermometer;
-Magnetic field meter (metal detector);
-Vibration level meter;
-Luminosity (LUX) level meter;
-Color sensor;
-Speedometer;
-Compass;
-Battery tester;
-Network speed test;
-Drag Racing.
-
-Other useful utilities kit:
-Unit currency and size converter;
-Calculator;
-Code scanner: QR code and bar code;
-Text scanner;
-NFC scanner;
-Accelerometer;
-Time zones;
-Mirror;
-Dog whistle;
-Microphone;
-Metronome;
-Pitch tuner;
-Counter;
-Random generator;
-Pedometer;
-Body mass index;
-Period tracker;
-Translator;
-Notepad.
-
-",
+        description: "Smart Tools - All In One is a useful kit of tools with 40+ carpenter construction measure and other tools and utilities. Use device's in-built sensors in all in one tool box app useful like a swiss army knife. Measure tools kit: dB level: Measure the sound dB level and its spectrum; Location (map) with altimeter; Distance meter; Stopwatch; Thermometer; Magnetic field meter (metal detector); Vibration level meter; Luminosity (LUX) level meter; Color sensor; Speedometer; Compass; Battery tester; Network speed test; Drag Racing. Other useful utilities kit: Unit currency and size converter; Calculator; Code scanner: QR code and bar code; Text scanner; NFC scanner; Accelerometer; Time zones; Mirror; Dog whistle; Microphone; Metronome; Pitch tuner; Counter; Random generator; Pedometer; Body mass index; Period tracker; Translator; Notepad.",
         link: "https://www.mediafire.com/file/sglj03dcq20a1tp/Smart+Tools-v20.9(Premium).apk"
     },
-     // Add more app data objects here if needed
+    // Add more app data objects here if needed
 ];
